92e11e72a61af039c8eec48795a6639214d25362
JF-Lalonde <lalondej88@gmail.com>
2017-06-20 11:55:21 -0600

    building out methods

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,36 +1,46 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+set :number, rand(100)
 
-before '/' do
-  params['guess'] ||= "default"
-end
+  get '/' do
+    guess = params['guess'].to_i
+    message = check_guess(guess)
+    reveal = ""
+    erb :index, :locals => {:reveal => reveal, :message => message}
+  end
 
-get '/' do
-  guess = params['guess'].to_i
-  message = check_guess(guess)
-  reveal = ""
-  erb :index, :locals => {:reveal => reveal, :message => message}
-end
 
-def check_guess(guess)
-  if guess.class != Fixnum
-    message = "whatever"
-  elsif guess > SECRET_NUMBER
-    if guess - SECRET_NUMBER > 5
+  def check_guess(guess)
+    if guess > settings.number
+      too_high(guess)
+    elsif guess < settings.number
+      too_low(guess)
+    elsif guess == settings.number
+      winner; show_answer
+    end
+  end
+
+  def too_high(guess)
+    if guess - settings.number > 5
       message = "Way too high!"
     else
       message = "Too High!"
     end
-  elsif guess < SECRET_NUMBER
-    if guess - SECRET_NUMBER < -5
+  end
+
+  def too_low(guess)
+    if guess - settings.number < -5
       message = "Way too low!"
     else
       message = "Too Low!"
     end
-  elsif guess == SECRET_NUMBER
-    message = "Nicely done!"
-    reveal = "The SECRET NUMBER is  #{SECRET_NUMBER}"
   end
-end
+
+  def winner
+    message = "Nicely done!"
+  end
+
+  def show_answer
+    reveal = "The SECRET NUMBER is  #{settings.number}"
+  end
