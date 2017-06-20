33256bf765b272a391f5a59c991a03def09b5d99
JF-Lalonde <lalondej88@gmail.com>
2017-06-19 23:20:47 -0600

    trying to prevent default dialogue from showing up

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,28 +1,36 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
+
+before '/' do
+  params['guess'] ||= "default"
+end
 
 get '/' do
   guess = params['guess'].to_i
-  # message = check_guess(guess)
-  if guess > number
-    if guess - number > 5
+  message = check_guess(guess)
+  reveal = ""
+  erb :index, :locals => {:reveal => reveal, :message => message}
+end
+
+def check_guess(guess)
+  if guess.class != Fixnum
+    message = "whatever"
+  elsif guess > SECRET_NUMBER
+    if guess - SECRET_NUMBER > 5
       message = "Way too high!"
     else
       message = "Too High!"
     end
-  elsif guess < number
-    if guess - number < -5
+  elsif guess < SECRET_NUMBER
+    if guess - SECRET_NUMBER < -5
       message = "Way too low!"
     else
       message = "Too Low!"
     end
-  elsif guess == number
-    message = "Right on brother!"
+  elsif guess == SECRET_NUMBER
+    message = "Nicely done!"
+    reveal = "The SECRET NUMBER is  #{SECRET_NUMBER}"
   end
-  erb :index, :locals => {:number => number, :message => message}
 end
-
-# def check_guess(guess)
-# end
