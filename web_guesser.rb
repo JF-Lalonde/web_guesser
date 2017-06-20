a5a52babfe53e9037ff9384ac0de2fedab5ae712
JF-Lalonde <lalondej88@gmail.com>
2017-06-19 20:14:41 -0600

    too high message working

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,13 @@ require 'sinatra/reloader'
 number = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess'].to_i
+  # message = check_guess(guess)
+  if guess > number
+    message = "Too High!"
+  end
+  erb :index, :locals => {:number => number, :message => message}
 end
+
+# def check_guess(guess)
+# end
