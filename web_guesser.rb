97ba202cd9d21fc777d0c3f1bcce4cd06c3efe5d
JF-Lalonde <lalondej88@gmail.com>
2017-06-20 13:10:45 -0600

    completed color extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,46 +1,46 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :number, rand(100)
+  set :number, rand(100)
 
   get '/' do
     guess = params['guess'].to_i
-    message = check_guess(guess)
-    reveal = ""
-    erb :index, :locals => {:reveal => reveal, :message => message}
+    message = check_guess(guess)[0]
+    reveal = check_guess(guess)[1]
+    color = check_guess(guess)[2]
+    erb :index, :locals => {reveal: reveal, message: message, color: color}
   end
-
 
   def check_guess(guess)
     if guess > settings.number
-      too_high(guess)
+      return too_high(guess)[0], " ", too_high(guess)[1]
     elsif guess < settings.number
-      too_low(guess)
+      return too_low(guess)[0], " ", too_low(guess)[1]
     elsif guess == settings.number
-      winner; show_answer
+      return winner[0], answer, winner[1]
     end
   end
 
   def too_high(guess)
     if guess - settings.number > 5
-      message = "Way too high!"
+      return "Way too high!", "red"
     else
-      message = "Too High!"
+      return "Too High!", "#F27989"
     end
   end
 
   def too_low(guess)
     if guess - settings.number < -5
-      message = "Way too low!"
+      return "Way too low!", "red"
     else
-      message = "Too Low!"
+      return "Too Low!", "#F27989"
     end
   end
 
   def winner
-    message = "Nicely done!"
+    return "Nicely done!", "green"
   end
 
-  def show_answer
-    reveal = "The SECRET NUMBER is  #{settings.number}"
+  def answer
+    "The SECRET NUMBER is  #{settings.number}"
   end
